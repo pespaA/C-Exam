cb96f5218d42a2e1bc1a6a183281741d636b741e
pespaA <omaramokhtar2@gmail.com>
2025-01-05 10:13:17 +0200

    Add files via upload

---
+++ ExamSystem/Subject.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExamSystem.Exams;
+using ExamSystem.Questions;
+
+namespace ExamSystem
+{
+    public class Subject
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public Exam Exams { get; set; }
+
+        public Subject(int _Id,string _Name,Exam _exam)
+        {
+            ID = _Id;
+            Name = _Name;
+            Exams = _exam;
+        }
+    }
+}
