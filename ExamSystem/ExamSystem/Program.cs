e715dc3958b74b0dbc94173005c6a329efb53053
pespaA <omaramokhtar2@gmail.com>
2025-01-05 21:24:53 +0200

    Add files via upload

---
+++ ExamSystem/ExamSystem/Program.cs
@@ -0,0 +1,66 @@
+﻿using ExamSystem.Exams;
+using ExamSystem.Questions;
+
+namespace ExamSystem
+{
+    internal class Program
+    {
+        public static int GetInt(int max = 999,int min = 0)
+        {
+            while (true)
+            {
+                string input = Console.ReadLine();
+                try
+                {
+                    int result = int.Parse(input);
+                    if (result >= min && result <= max )
+                        return int.Parse(input);
+                    else
+                        Console.WriteLine("Invalid input. Please try again.");
+                }
+                catch
+                {
+                    Console.WriteLine("Invalid input. Please try again.");
+                }
+            }
+        }
+        public static bool GetBool()
+        {
+            while (true)
+            {
+                string input = Console.ReadLine();
+                try { bool result = bool.Parse(input); return bool.Parse(input); }
+                catch { Console.WriteLine("Invalid input. Please try again."); }
+            }
+        }
+        static void Main(string[] args)
+        {
+            int time;
+            int NumberOfQustions;
+            int i = -1;
+            while (i != 0)
+            {
+                Console.WriteLine("Please Enter The Type Of Exam \n1 for Practical Exam \n2 For Final Exam\n0 to close");
+                i = GetInt(2,0);
+                if (i == 1)
+                {
+                    Console.WriteLine("Please Enter Time For Exam 30 min to 180 min");
+                    time = GetInt(180,30);
+                    Console.WriteLine("Please Enter The Number Of Qustions");
+                    NumberOfQustions = GetInt();
+                    Exam exam = new PracticalExam(time,NumberOfQustions);
+                    exam.ShowExam();
+                }
+                if (i == 2)
+                { 
+                    Console.WriteLine("Please Enter Time For Exam 30 min to 180 min");
+                    time = GetInt(180, 30);
+                    Console.WriteLine("Please Enter The Number Of Qustions");
+                    NumberOfQustions = GetInt();
+                    Exam exam = new FinalExam(time, NumberOfQustions);
+                    exam.ShowExam();
+                }
+            }
+        }
+    }
+}
---
+++ ExamSystem/ExamSystem/Questions/TrueFalse.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamSystem.Questions
+{
+    internal class TrueFalse : Question
+    {
+        public bool CorrectAnswer { get; set; }
+        public TrueFalse(string _Header, string _Body, int _Mark, bool _CorrectAnswer) : base(_Header, _Body, _Mark)
+        {
+            CorrectAnswer = _CorrectAnswer;
+        }
+        public override string ToString()
+        {
+            return $"{Header}     Mark:{Mark}\n{Body}";
+        }
+    }
+}
